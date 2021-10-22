0faae9ec6b9ad4888e25012f4dc86f7a72a6b6ec
Ngô Nhật Minh <minhngo171997@gmail.com>
2021-10-22 08:12:21 +0700

    ok

--- back_end/src/main/java/com/example/project/repository/GarageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.project.repository;
-
-import com.example.project.model.Garage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GarageRepository extends JpaRepository<Garage, Long> {
-}
